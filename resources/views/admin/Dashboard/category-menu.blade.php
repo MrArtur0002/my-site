bb8ca3bbd44dbed67c4913c18d47237ea8f80815
MrArtur0074 <smiteartur01@mail.ru>
2021-08-30 07:53:40 +0500

    Добавление миграций карточек товара. Добавление раздела в админке.

---
+++ resources/views/admin/Dashboard/category-menu.blade.php
@@ -0,0 +1,14 @@
+<div class="menu-block">
+    <div class="category-title">Категории:</div>
+    <div class="d-flex justify-content-center">
+        <ul>
+            <li><a href="/admin">Редактор страниц</a></li>
+            <li><a href="/admin/munchkin">Манчкин</a></li>
+            <li><a href="#">Категория 3</a></li>
+            <li><a href="#">Категория 4</a></li>
+            <li><a href="#">Категория 5</a></li>
+            <li><a href="#">Категория 6</a></li>
+            <li><a href="#">Создать новую карту</a></li>
+        </ul>
+    </div>
+</div>
---
+++ resources/views/admin/Dashboard/munchkin.blade.php
@@ -0,0 +1,42 @@
+<div class="d-flex flex-column f-height">
+    <div class="munchkin-top-menu d-lfex">
+        <ul>
+            <li><a href="/admin/munchkin"> Главная</a></li>
+            <li><a href="/admin/munchkin/card"> Карточки</a></li>
+            <li><a href="#"> Игроки</a></li>
+            <li><a href="#"> Режимы игры</a></li>
+        </ul>
+    </div>
+    <div class="munchkin-content flex-fill">
+        <div class="munchkin-filters">
+            Фильтры (в будущем ближайшем)
+        </div>
+        <div class="row cards">
+            <div class="col-4">
+                <div class="card">
+
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card">
+
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card">
+
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card">
+
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="card">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
